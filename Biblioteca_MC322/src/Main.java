04658844c3daca409fe6797e1815fbf32f1ed5b3
yurir <yurir@100.112.122.110>
2023-08-14 21:58:25 -0300

    14/08

--- Biblioteca_MC322/src/Main.java
+++ Biblioteca_MC322/src/Main.java
@@ -2,6 +2,7 @@
 import materiais.Livros;
 import membros.Funcionarios;
 import membros.Usuarios;
+import materiais.Periodicos;
 
 /* LAB1
 Wanderley Pedroso da Graca Barion RA: 225790
@@ -16,11 +17,13 @@ public class Main {
 		Livros livro1 = new Livros("Dom Quixote", "Miguel de Cervantes", 250, 1);
 		Funcionarios funcionario1 = new Funcionarios("Joao Silva", "Bibliotecario", 001, "123.456.789-12");
 		Usuarios usuario1 = new Usuarios("Ana Maria", "Aluno", 00001, "987.654.321-98");
+		Periodicos pediodico1= new Periodicos ("News14/08", "Jorge Mendes",100);
 	
 	// impressao dos objetos
 		System.out.println("Primeiro livro:\n"+livro1);
 		System.out.println("Primeiro funcionário:\n"+funcionario1);
 		System.out.println("Primeiro usuário:\n"+usuario1);
+		System.out.println("Primeiro periodico:\n"+ pediodico1);
 	}
 	
 
