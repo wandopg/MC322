a2f98ff80d6e11d311e7b1ec63c5b9816b77e5dc
wanderleypedroso <wanderleypedroso@fedora>
2023-08-14 21:58:08 -0300

    14/08

--- Biblioteca_MC322/src/Main.java
+++ Biblioteca_MC322/src/Main.java
@@ -1,5 +1,6 @@
 
 import materiais.Livros;
+import materiais.Teses;
 import membros.Funcionarios;
 import membros.Usuarios;
 
@@ -14,11 +15,13 @@ public class Main {
 	
 	//Instanciando objetos
 		Livros livro1 = new Livros("Dom Quixote", "Miguel de Cervantes", 250, 1);
+		Teses tese1 = new Teses("POO em MC322", "Bruno Cafeo", 375);
 		Funcionarios funcionario1 = new Funcionarios("Joao Silva", "Bibliotecario", 001, "123.456.789-12");
 		Usuarios usuario1 = new Usuarios("Ana Maria", "Aluno", 00001, "987.654.321-98");
 	
 	// impressao dos objetos
 		System.out.println("Primeiro livro:\n"+livro1);
+		System.out.println("Priemira Tese:\n"+tese1);
 		System.out.println("Primeiro funcionário:\n"+funcionario1);
 		System.out.println("Primeiro usuário:\n"+usuario1);
 	}
