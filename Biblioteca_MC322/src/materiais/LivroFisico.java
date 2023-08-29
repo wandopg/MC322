ddd77dd6b621b9f1d85f6faa20b4bfa381e74d58
yurir <yurir@100.112.35.181>
2023-08-28 22:38:32 -0300

    t

--- Biblioteca_MC322/src/materiais/LivroFisico.java
+++ Biblioteca_MC322/src/materiais/LivroFisico.java
@@ -1,46 +1,112 @@
 package materiais;
 
-public class LivroFisico extends ItemMultimidia {
+public class LivroFisico extends ItenMultimidia {
 
-	private int paginas;
-	private int tombo;
+	private int isbn;
+	private int edicao;
+	private int copias;
+	private int copiasDisponiveis;
+	private String localizacao;
+	private String estado;
+	
+	
 	
 	// Metodo construtor
-
-	public LivroFisico(String titulo, String autor, int paginas, int tombo) {
-		super(autor,titulo);
-		this.paginas = paginas;
-		this.tombo = tombo;
+	
+	public LivroFisico(String titulo, String autorOuArtista, String editoraOuGravdora, int anoPubli, String genero,
+			String sinopse, int isbn, int edicao, int copias, int copiasDisponiveis, String localizacao,
+			String estado) {
+		super(titulo, autorOuArtista, editoraOuGravdora, anoPubli, genero, sinopse);
+		this.isbn = isbn;
+		this.edicao = edicao;
+		this.copias = copias;
+		this.copiasDisponiveis = copiasDisponiveis;
+		this.localizacao = localizacao;
+		this.estado = estado;
 	}
-		
-	// Outros metodos
-		
+
+
+	// Metodos Get and Set
+	
+	public int getIsbn() {
+		return isbn;
+	}
+
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+
+	public int getEdicao() {
+		return edicao;
+	}
+
+
+	public void setEdicao(int edicao) {
+		this.edicao = edicao;
+	}
+
+	
+	public int getCopias() {
+		return copias;
+	}
+
+
+	public void setCopias(int copias) {
+		this.copias = copias;
+	}
+
+	
+	public int getCopiasDisponiveis() {
+		return copiasDisponiveis;
+	}
+
+
+	public void setCopiasDisponiveis(int copiasDisponiveis) {
+		this.copiasDisponiveis = copiasDisponiveis;
+	}
+
+	
+	public String getLocalizacao() {
+		return localizacao;
+	}
+
+
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+
+	
+	public String getEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	// String 
+	
 	public String toString() {
-		String out = "Titulo = "+getTitulo()+"\n";
-		out = out + "Autor = "+getAutor()+"\n";
-		out = out + "Numero de paginas = "+getPaginas()+"\n";
-		out = out + "Tombo = "+getTombo()+"\n";
+		String out = "Titulo: "+getTitulo()+"\n";
+		out = out + "Autor = " + getAutorOuArtista() +"\n";
+		out = out + "Editor ou Gravadora = " + getEditoraOuGravdora() +"\n";
+		out = out + "Ano de publicacao = " + getAnoPubli() +"\n";
+		out = out + "Genero = " + getGenero() +"\n";
+		out = out + "Sinopse = " + getSinopse() +"\n";
+		out = out + "Isbn = " + getIsbn() +"\n";
+		out = out + "Isbn = " + getEdicao() +"\n";
+		out = out + "Isbn = " + getCopias() +"\n";
+		out = out + "Isbn = " + getCopiasDisponiveis() +"\n";
+		out = out + "Isbn = " + getLocalizacao() +"\n";
+		out = out + "Isbn = " + getEstado() +"\n";
+		
 		return out;
 	}
 	
-	// Metodos Get and Set 
-		
-	//Paginas
-	public int getPaginas() {
-		return paginas;
-	}
-		
-	public void setPaginas(int paginas) {
-		this.paginas = paginas;
-	}
 	
-	//Tombo
-	public int getTombo() {
-		return tombo;
-	}
-		
-	public void setTombo(int tombo) {
-		this.tombo = tombo;
-	}	
 
 }
