60bbfe07596c643815290e65b0cd846b53f44476
yurir <yurir@J-01>
2023-09-10 23:10:55 -0300

    t

--- Biblioteca_MC322/src/Main.java
+++ Biblioteca_MC322/src/Main.java
@@ -16,20 +16,10 @@ public class Main {
 	public static void main(String[] args) {
 	
 	//Instanciando objetos
-		LivroFisico livrofisico1 = new LivroFisico("Dom Quixote", "Miguel de Cervantes", 250, 1);
-		LivroEletronico livroeletronico1 = new LivroEletronico("Dom Quixote", "Miguel de Cervantes", 250, "www.livro1.com.br");
-		Funcionarios funcionario1 = new Funcionarios("Joao Silva", "Bibliotecario", 001, "123.456.789-12");
-		EstudantesGraduacao estudante1 = new EstudantesGraduacao("Joao Silva", 123456, "123.456.789-12");
-		Cd cd1 = new Cd("Dom Quixote - Audiolivro", "Miguel de Cervantes", 15, 120);
-		Dvd dvd1 = new Dvd("Dom Quixote", "Miguel de Cervantes", 133);
+		
 		
 	// impressao dos objetos
-		System.out.println("Primeiro livro fisico:\n"+livrofisico1);
-		System.out.println("Primeiro livro eletronico:\n"+livroeletronico1);
-		System.out.println("Primeiro cd:\n"+cd1);
-		System.out.println("Primeiro dvd:\n"+dvd1);
-		System.out.println("Primeiro estudante:\n"+estudante1);
-		System.out.println("Primeiro funcionário:\n"+funcionario1);
+		
 	}
 	
 
